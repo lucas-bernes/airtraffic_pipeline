91bf17d7d6b3fe82c3d31c325d76e84ce2254d0d
Lucas Bernes <bernesi.bsni@gmail.com>
2026-06-16 08:50:17 -0300

    fix: fix incorrect number of collumns in schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,11 +1,11 @@
-CREATE DATABASE IF NOT EXISTS flight_tracker;
-
-USE flight_tracker;
-
-CREATE TABLE IF NOT EXISTS flights (
+CREATE TABLE IF NOT EXISTS flight_tracker (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    datetime DATETIME,
+    timestamp DATETIME,
     icao24 VARCHAR(20),
     callsign VARCHAR(20),
-    origin_country VARCHAR(100)
+    origin_country VARCHAR(100),
+    latitude DOUBLE,
+    longitude DOUBLE,
+    altitude DOUBLE,
+    velocity DOUBLE
 );
