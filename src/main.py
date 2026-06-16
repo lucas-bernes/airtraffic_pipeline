9194c20e5aafeaef00bac02ccfb5a79850e6525b
Lucas Bernes <bernesi.bsni@gmail.com>
2026-06-16 06:57:55 -0300

    feat: load opensky data using api

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 from dotenv import load_dotenv
 import os
+import requests
+import pprint
 
 load_dotenv()
 
 api_key = os.getenv('API_KEY')
 
-print(api_key)
+url = "https://opensky-network.org/api/states/all"
+
+response = requests.get(url)
+data = response.json()
+pprint.pprint(data['states'][:10])
